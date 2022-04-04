bf6e90d5bb8bf79b5038d2d2f251639c82154388
Nathan LeClaire <nathan.leclaire@gmail.com>
2022-04-04 15:49:57 -0700

    Follow along with transactions
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

--- src/renderer/components/TransactionView.tsx
+++ src/renderer/components/TransactionView.tsx
@@ -7,12 +7,36 @@ function TransactionView(props: { net: Net }) {
   const { net } = props;
   const [latestProcessedSlot, setLatestProcessedSlot] = useState(0);
   useEffect(() => {
-    const solConn = new sol.Connection(netToURL(net));
-    (async () => {
+    const blockInterval = setInterval(async () => {
+      const solConn = new sol.Connection(netToURL(net));
       const blockProduction = await solConn.getBlockProduction();
+      const blocks = await solConn.getBlocks(
+        latestProcessedSlot,
+        blockProduction.context.slot - 1
+      );
+      blocks.forEach(async (block) => {
+        const blockDetails = await solConn.getBlock(block);
+        blockDetails?.transactions.forEach(async (txn) => {
+          const parsedTxn = await solConn.getParsedTransactions(
+            txn.transaction.signatures
+          );
+          const instructions = parsedTxn[0]?.transaction.message.instructions;
+          if (instructions) {
+            const parsedInstruction = instructions[0] as sol.ParsedInstruction;
+            if (parsedInstruction.program !== 'vote') {
+              console.log(parsedInstruction);
+            }
+          }
+        });
+      });
       setLatestProcessedSlot(blockProduction.context.slot);
-    })();
-  }, [net]);
+    }, 1000);
+
+    return () => {
+      window.clearInterval(blockInterval);
+    };
+  }, [latestProcessedSlot, net]);
+
   return <>{latestProcessedSlot}</>;
 }
 export default TransactionView;
